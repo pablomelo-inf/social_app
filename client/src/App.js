85174d67bbad086ebd6ce5b584e352a7689c85e5
pablomelo-inf <pablomelo.inf@gmail.com>
2024-05-01 23:18:34 -0300

    creat register,  login pages and form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
+import {BrowserRouter,  Routes, Route} from 'react-router-dom';
 import  HomePage from './scenes/homePage';
 import LoginPage from './scenes/loginPage';
 import ProfilePage from './scenes/profilePage';
@@ -8,11 +8,9 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from './theme';
 
-
 function App() {  
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
 
   return( 
     <div className="App">
